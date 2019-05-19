414009292cea3a0e286cff92064113309bf843f8
Mohammad Abdur Razzaque <hiraz339@gmail.com>
2019-05-19 20:52:38 +0100

    Add files via upload

---
+++ Chapter4-Speech-Speaker-recognition/Use-case-1/wave2img.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 13:33:51 2019
+
+@author: raz
+"""
+
+import os
+import wave
+
+Dir= 'dataset-wav/00b01445_nohash_0.wav'
+
+import pylab
+def graph_spectrogram(wav_file):
+    sound_info, frame_rate = get_wav_info(wav_file)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    pylab.title('spectrogram of %r' % wav_file)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram.png')
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+
+graph_spectrogram(Dir)
+get_wav_info(Dir)
